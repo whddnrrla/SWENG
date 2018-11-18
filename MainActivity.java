8cd0311f4734d16492ec37e4be8d5130d54d3552
calf0530 <44886069+calf0530@users.noreply.github.com>
2018-11-18 22:25:20 +0900

    Add files via upload
    제가 한 부분까지 올렸어요.

---
+++ MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.lenovo.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etEmail;
+    private Button btnRegist;
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        btnRegist = (Button) findViewById(R.id.btnRegist);
+        btnRegist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegistActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                startActivityForResult(intent, 1000);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Log.d("RESULT", requestCode + "");
+        Log.d("RESULT", resultCode + "");
+        Log.d("RESULT", data + "");
+
+        if (requestCode == 1000 && resultCode == RESULT_OK) {
+            Toast.makeText(MainActivity.this, "회원가입을 완료했습니다!", Toast.LENGTH_SHORT).show();
+            etEmail.setText(data.getStringExtra("email"));
+        }
+    }
+
+}
